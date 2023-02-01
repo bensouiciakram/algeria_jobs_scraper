2c77b2c9de6bc0d993e4d0671e06b90ed1638be8
bensouici akram <bensouiciakram@gmail.com>
2023-02-01 17:15:31 +0100

    implementing bayt part of extracting

--- scraper.py
+++ scraper.py
@@ -67,7 +67,8 @@ class InfosSpider(scrapy.Spider):
             )
 
     def parse_jobs(self,response):
-        jobs_urls = set(response.xpath(self.config['emploitic.com']['xpaths']['job_urls']).getall())
+        jobs_urls = set(response.xpath(self.config[response.meta.get('domain')]['xpaths']['jobs_urls']).getall()) if not self.config[response.meta.get('domain')] else \
+            [response.urljoin(url) for url in response.xpath(self.config[response.meta.get('domain')]['xpaths']['jobs_urls']).getall()]
         for url in jobs_urls :
             yield Request(
                 url,
@@ -81,31 +82,30 @@ class InfosSpider(scrapy.Spider):
         domain = response.meta.get('domain')
         loader = ItemLoader(DetailsItem(),response)
         loader.add_value('job_url',response.url)
-        loader.add_xpath('title',self.config[domain]['xpaths']['title'])
-        loader.add_xpath('work_place',self.config[domain]['xpaths']['work_place'])
-        loader.add_xpath('expiration_date',self.config[domain]['xpaths']['expiration_date'])
-        loader.add_xpath('applicant_level',self.config[domain]['xpaths']['applicant_level'])
-        loader.add_xpath('section',self.config[domain]['xpaths']['section'])
-        loader.add_xpath('number_of_available_jobs',self.config[domain]['xpaths']['number_of_available_jobs'])
-        loader.add_xpath('contracts_type',self.config[domain]['xpaths']['contracts_type'])
-        loader.add_xpath('description',self.config[domain]['xpaths']['description'])
+        loader.add_xpath('title',self.config[domain]['xpaths']['title']) if self.config[domain]['xpaths']['title'] else None 
+        loader.add_xpath('work_place',self.config[domain]['xpaths']['work_place']) if self.config[domain]['xpaths']['work_place'] else None 
+        loader.add_xpath('expiration_date',self.config[domain]['xpaths']['expiration_date']) if self.config[domain]['xpaths']['expiration_date'] else None 
+        loader.add_xpath('applicant_level',self.config[domain]['xpaths']['applicant_level']) if self.config[domain]['xpaths']['applicant_level'] else None 
+        loader.add_xpath('section',self.config[domain]['xpaths']['section']) if self.config[domain]['xpaths']['section'] else None 
+        loader.add_xpath('number_of_available_jobs',self.config[domain]['xpaths']['number_of_available_jobs']) if self.config[domain]['xpaths']['number_of_available_jobs'] else None 
+        loader.add_xpath('contracts_type',self.config[domain]['xpaths']['contracts_type']) if self.config[domain]['xpaths']['contracts_type'] else None 
+        loader.add_xpath('description',self.config[domain]['xpaths']['description']) if self.config[domain]['xpaths']['description'] else None 
         yield loader.load_item()
 
-    
+
+if __name__ == '__main__':
+    keyword = input('what keyword you want to choose in your search : ').strip()
+    process = CrawlerProcess(
+        {
+            'FEED_URI':'output.csv',
+            'FEED_FORMAT':'csv',
+            #'LOG_LEVEL':'ERROR',
+            'HTTPCACHE_ENABLED' : True,
+            'USER_AGENT':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+        }
+    )
 
 
-keyword = input('what keyword you want to choose in your search : ').strip()
-    
-process = CrawlerProcess(
-    {
-        'FEED_URI':'output.csv',
-        'FEED_FORMAT':'csv',
-        'HTTPCACHE_ENABLED' : True,
-        'USER_AGENT':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
-    }
-)
 
-
-
-process.crawl(InfosSpider,keyword)
-process.start()
+    process.crawl(InfosSpider,keyword)
+    process.start()
